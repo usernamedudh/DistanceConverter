5cd9f91af61be68353446831c592bba7c06e1a8b
usernamedudh <vampwave2@gmail.com>
2025-06-28 23:30:48 +0300

    Added milesToKm and kmToMiles functions

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,9 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Конвертер відстані: милі - кілометри");
+        System.out.println("10 миль = " + milesToKm(10) + " км");
+        System.out.println("16 км = " + kmToMiles(16) + " миль");
+
     }
 
     public static double milesToKm(double miles) {
